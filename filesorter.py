656d8f8f4c11685d3294c1c6d30d96bbc3feeb2c
Adam Fischbein <adamfischbein2005@gmail.com>
2025-11-17 17:14:21 +0100

    improved dictionary and if statements

--- filesorter.py
+++ filesorter.py
@@ -16,10 +16,10 @@ files = os.listdir(location)
 files = [f for f in files if os.path.isfile(location + "/" + f)]
 
 file_types = {"IMAGE": ["png", "jpg"],
-              "VIDEO": "video",
-              "TEXT": "txt",
-              "CSV": "csv",
-              "JSON": "json"
+              "VIDEO": ["video"],
+              "TEXT": ["txt"],
+              "CSV": ["csv"],
+              "JSON": ["json"]
               }
 
 for file in files:
@@ -27,11 +27,11 @@ for file in files:
 
     if parts[1] in file_types.get("IMAGE"):
         print(f"{file} -> {parts[1]}")
-    # if parts[1] == file_types.get("VIDEO"):
-    #     print(f"{file} -> {parts[1]}")
-    # if parts[1] == file_types.get("TEXT"):
-    #     print(f"{file} -> {parts[1]}")
-    # if parts[1] == file_types.get("JSON"):
-    #     print(f"{file} -> {parts[1]}")
-    # if parts[1] == file_types.get("CSV"):
-    #     print(f"{file} -> {parts[1]}")
+    if parts[1] in file_types.get("VIDEO"):
+        print(f"{file} -> {parts[1]}")
+    if parts[1] in file_types.get("TEXT"):
+        print(f"{file} -> {parts[1]}")
+    if parts[1] in file_types.get("JSON"):
+        print(f"{file} -> {parts[1]}")
+    if parts[1] in file_types.get("CSV"):
+        print(f"{file} -> {parts[1]}")
