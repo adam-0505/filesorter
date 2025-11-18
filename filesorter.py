01ab4e4c747556ad9eff195064ebefb06908ee45
Adam Fischbein <adamfischbein2005@gmail.com>
2025-11-18 20:28:13 +0100

    program creates folders based on file type

--- filesorter.py
+++ filesorter.py
@@ -26,12 +26,46 @@ for file in files:
     parts = file.split(".")
 
     if parts[1] in file_types.get("IMAGE"):
-        print(f"{file} -> {parts[1]}")
+        # print(f"{file} -> {parts[1]}")
+        if os.path.exists(location + "/IMAGES"):
+            # move file into folder
+            pass        
+        else:
+            os.mkdir(location + "/IMAGES")
+            # move file into new folder
+            
     if parts[1] in file_types.get("VIDEO"):
-        print(f"{file} -> {parts[1]}")
+        # print(f"{file} -> {parts[1]}")
+        if os.path.exists(location + "/VIDEOS"):
+            # move file into folder
+            pass
+        else:
+            os.mkdir(location + "/VIDEOS")
+            # move file into new folder
+
     if parts[1] in file_types.get("TEXT"):
-        print(f"{file} -> {parts[1]}")
+        # print(f"{file} -> {parts[1]}")
+        if os.path.exists(location + "/TEXT_FILES"):
+            # move file into folder
+            pass
+        else:
+            os.mkdir(location + "/TEXT_FILES")
+            # move file into new folder
+
     if parts[1] in file_types.get("JSON"):
-        print(f"{file} -> {parts[1]}")
+        # print(f"{file} -> {parts[1]}")
+        if os.path.exists(location + "/JSON_FILES"):
+            # move file into folder
+            pass
+        else:
+            os.mkdir(location + "/JSON_FILES")
+            # move file into new folder  
+                      
     if parts[1] in file_types.get("CSV"):
-        print(f"{file} -> {parts[1]}")
+        # print(f"{file} -> {parts[1]}")
+        if os.path.exists(location + "/CSV_FILES"):
+            # move file into folder
+            pass
+        else:
+            os.mkdir(location + "/CSV_FILES")
+            # move file into new folder
