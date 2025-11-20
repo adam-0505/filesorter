a214341e133a71a59ad721b370452fc4f5f9c1b9
Adam Fischbein <adamfischbein2005@gmail.com>
2025-11-21 00:01:15 +0100

    added message for files that do not match any known file type

--- filesorter.py
+++ filesorter.py
@@ -20,53 +20,67 @@ file_types = {"IMAGE": ["png", "jpg"],
 
 for file in files:
     parts = file.split(".")
+    known = False
 
     if parts[1] in file_types.get("IMAGE"):
         # print(f"{file} -> {parts[1]}")
         if os.path.exists(f"{location}/IMAGES"):
             shutil.move(f"{location}/{file}", f"{location}/IMAGES") 
             print(f"Moved {file} to {location}/IMAGES")
+            known = True
         else:
             os.mkdir(f"{location}/IMAGES")
             shutil.move(f"{location}/{file}", f"{location}/IMAGES")
             print(f"Moved {file} to {location}/IMAGES")
+            known = True
 
     if parts[1] in file_types.get("VIDEO"):
         # print(f"{file} -> {parts[1]}")
         if os.path.exists(f"{location}/VIDEOS"):
             shutil.move(f"{location}/{file}", f"{location}/VIDEOS")
             print(f"Moved {file} to {location}/VIDEOS")
+            known = True
         else:
             os.mkdir(f"{location}/VIDEOS")
             shutil.move(f"{location}/{file}", f"{location}/VIDEOS")
             print(f"Moved {file} to {location}/VIDEOS")
+            known = True
 
     if parts[1] in file_types.get("TEXT"):
         # print(f"{file} -> {parts[1]}")
         if os.path.exists(f"{location}/TEXT_FILES"):
             shutil.move(f"{location}/{file}", f"{location}/TEXT_FILES")
             print(f"Moved {file} to {location}/TEXT_FILES")
+            known = True
         else:
             os.mkdir(f"{location}/TEXT_FILES")
             shutil.move(f"{location}/{file}", f"{location}/TEXT_FILES")
             print(f"Moved {file} to {location}/TEXT_FILES")
+            known = True
 
     if parts[1] in file_types.get("JSON"):
         # print(f"{file} -> {parts[1]}")
         if os.path.exists(f"{location}/JSON_FILES"):
             shutil.move(f"{location}/{file}", f"{location}/JSON_FILES")
             print(f"Moved {file} to {location}/JSON_FILES")
+            known = True
         else:
             os.mkdir(f"{location}/JSON_FILES")
             shutil.move(f"{location}/{file}", f"{location}/JSON_FILES")  
             print(f"Moved {file} to {location}/JSON_FILES")
+            known = True
                       
     if parts[1] in file_types.get("CSV"):
         # print(f"{file} -> {parts[1]}")
         if os.path.exists(f"{location}/CSV_FILES"):
             shutil.move(f"{location}/{file}", f"{location}/CSV_FILES")
             print(f"Moved {file} to {location}/CSV_FILES")
+            known = True
         else:
             os.mkdir(f"{location}/CSV_FILES")
             shutil.move(f"{location}/{file}", f"{location}/CSV_FILES")
             print(f"Moved {file} to {location}/CSV_FILES")
+            known = True
+
+    if not known:
+        print(f"Unknown '{file}'")
